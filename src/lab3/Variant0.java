c4fa00cabce2e70ebe8f02bd39f8ecef6d8260a9
dymik739 <user@109.86.70.81>
2023-05-11 15:50:03 +0300

    add my attempt at doing lab3

--- src/lab3/Variant0.java
+++ src/lab3/Variant0.java
@@ -1,17 +1,81 @@
 package lab3;
 
+import java.util.Set;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+
+class WordCounter {
+    private String word;
+    private int counter = 0;
+
+    public void main() {}
+
+    public WordCounter(String word) {
+        this.word = word;
+    }
+
+    public void increment() {
+        this.counter += 1;
+    }
+
+    public String get_word() {
+        return this.word;
+    }
+
+    public int get_result() {
+        return this.counter;
+    }
+}
+
 
 /**
  * Знайти найбільшу кількість речень заданого тексту, в яких є однакові слова.
  */
 public class Variant0 {
     public static void main(String[] args) {
-        String textString = "A, a. B a. B a. B a. C.";
-        final String[] sentencesStrings = textString.split("\\. ?");
+        String textString = "A, a. B a. B a? B a! Ccccc.";
+        final String[] sentencesStrings = textString.split("\\[.?!] ?");
+
         for (String sentencesString : sentencesStrings) {
             System.out.println(sentencesString);
         }
+
+        ArrayList<WordCounter> wordCounter = new ArrayList<WordCounter>();
+
+        for (String sentence : sentencesStrings) {
+            String[] localWords = sentence.split(",? ");
+
+word_adder:for (String word : localWords) {
+                for (WordCounter w : wordCounter) {
+                    if (w.get_word().equals(word)) {
+                        break word_adder;
+                    }
+
+                    wordCounter.add(new WordCounter(word));
+                }
+            }
+        }
+
+        for (String sentence : sentencesStrings) {
+            Set<String> localWords = new HashSet(Arrays.asList(sentence.split(",? ")));
+            
+            for (WordCounter wc : wordCounter) {
+                if (localWords.contains(wc.get_word())) {
+                    wc.increment();
+                }
+            }
+        }
+        
+        WordCounter mostFrequentWord = new WordCounter("123");
+        for (WordCounter wc : wordCounter) {
+            if (mostFrequentWord.get_result() > wc.get_result()) {
+                mostFrequentWord = wc;
+            }
+        }
+
+        System.out.println("Most frequent word: " + mostFrequentWord.get_result());
+
 //        System.out.println(Arrays.toString(sentencesStrings));
         System.out.println("++++++++++++");
     }
